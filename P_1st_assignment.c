1e288d5abef943b2e4a611878aa32adddfe69ba2
HyuckJuneHong <31675711+HyuckJuneHong@users.noreply.github.com>
2017-09-06 10:30:30 +0900

    P_1st_assignment

---
+++ P_1st_assignment.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+		
+	printf("학번 : 2017301085\n");
+	printf("이름 : 홍혁준\n");
+	printf("번호 : 01055067988\n");
+	printf("메일 : jjj8337721@naver.com\n");
+
+	return 0;
+}
